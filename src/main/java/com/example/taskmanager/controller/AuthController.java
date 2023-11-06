1294bc6ab5db339e65a054e2e2fe8a3952d954c2
Piniekf <piniekf@gmail.com>
2023-11-06 08:46:12 +0100

    .

--- src/main/java/com/example/taskmanager/controller/AuthController.java
+++ src/main/java/com/example/taskmanager/controller/AuthController.java
@@ -22,22 +22,19 @@ public class AuthController {
         this.userService = userService;
     }
 
-    // handler method to handle home page request
     @GetMapping("/index")
     public String home(){
         return "index";
     }
 
-    // handler method to handle user registration form request
+
     @GetMapping("/register")
     public String showRegistrationForm(Model model){
-        // create model object to store form data
         UserDto user = new UserDto();
         model.addAttribute("user", user);
         return "register";
     }
 
-    // handler method to handle user registration form submit request
     @PostMapping("/register/save")
     public String registration(@Valid @ModelAttribute("user") UserDto userDto,
                                BindingResult result,
@@ -57,14 +54,12 @@ public class AuthController {
         userService.saveUser(userDto);
         return "redirect:/register?success";
     }
-    // handler method to handle list of users
     @GetMapping("/users")
     public String users(Model model){
         List<UserDto> users = userService.findAllUsers();
         model.addAttribute("users", users);
         return "users";
     }
-    // handler method to handle login request
     @GetMapping("/login")
     public String login(){
         return "login";
